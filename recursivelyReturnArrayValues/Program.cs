51c7dc4d364be4bdc17c83bcb8e7198fe260ccec
gogoshbelichi <gogoshbelichi@users.noreply.github.com>
2024-11-26 09:53:06 +0300

    Add project files.

---
+++ recursivelyReturnArrayValues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int i = 0;
+        int[] nums = { 7685, 98342, 12635, 000000, 988877665, 666, 7777, 88888, 918273645 };
+        void Foo(int i)
+        {
+            if (i == nums.Length) return;
+            Console.WriteLine(nums[i]);
+            i++;
+            Foo(i);
+        }
+        Foo(i);
+    }
+}
